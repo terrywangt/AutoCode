dd2ff17bddb94b87aa3108d590188a4c8b3bfc15
wangt <654497606@qq.com>
2019-07-30 15:15:32 +0800

    更新

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-
 require("./lib").run({
-    host:'127.0.0.1',
-    username:'root',
-    pwd:'wangtian',
-    storage:'test',
-    Templates:'./test/tpl/',
-    schema
-},function(){
-    console.log(JSON.stringify(arguments))
-},function(){
-    console.log(JSON.stringify(arguments))});
+    host:'',
+    username:'',
+    pwd:'',
+    storage:'',
+    templates:'./test/tpl/',  //模板目录
+    output:'./test/output/',  //输出目录    
+    tables:['tb_bill_payment_info'],
+    tablesConfig:{
+        'tb_bill_payment_info':{'comment':'测试表'}
+    }
+},function(tablesData){
+    //输出表数据
+    //console.log(JSON.stringify(arguments[0]))
+},function(err){
+   console.log(JSON.stringify(err))
+});
